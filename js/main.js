c603725b11466b98125a8dc703fdb4ed69330b20
Anuj Rathore <ajrathore1800@gmail.com>
2023-06-13 17:26:38 +0530

    nav upgrade

---
+++ js/main.js
@@ -0,0 +1,20 @@
+let navBtn = document.getElementById("navbtn");
+let navurls = document.getElementById("navurls")
+let logo = document.getElementById("logo")
+
+navBtn.addEventListener('click',()=>{
+
+    if(logo.style.display !== "none"){
+        logo.style.display = "none";
+        logo.style.marginLeft="-200px";
+        navurls.style.display="flex";
+        navurls.style.marginRight="100px"
+    }
+    else{
+        logo.style.display = "block";
+        logo.style.marginLeft = "0px";
+        navurls.style.display="none";
+        navurls.style.marginRight="100px"
+    }
+   
+})
